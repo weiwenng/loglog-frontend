6817097fcf4c032891fa6796eade27ba7f2c56b4
weiwen <weiwen.97@hotmail.com>
2022-07-15 00:21:12 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,15 @@
 <template>
     <!-- <Button /> -->
     <div>
-        <p> hello loggers </p>
-        <p> for fooditem list & buffet packages, pls go to menu tab </p>
-        <p> to input sales order, pls go to sales tab </p>
-        <p> for entire order list, pls go to all orders tab </p>
+        <p> hello loggers!! </p>
+        <p> for fooditem list & buffet packages, pls go to <span class="underline underline-offset-2 font-bold text-blue-800">menu</span> tab </p>
+        <p> to input sales order, pls go to <span class="underline underline-offset-2 font-bold text-blue-800">sales</span> tab </p>
+        <p> for entire order list, pls go to all <span class="underline underline-offset-2 font-bold text-blue-800">orders</span> tab </p>
         <p> can also click on more details to find more deets for the order!! </p>
-        <p> to see all orders within a specific time period, pls go to orders by date tab </p>
-        <p> for logistics list, pls go to warehouse tab (red bar at the bottom means low in stock!!!! time to buy!!! ) </p>
+        <p> to see all orders within a specific time period, pls go to <span class="underline underline-offset-2 font-bold text-blue-800">orders by date</span> tab </p>
+        <p> for logistics, pls go to <span class="underline underline-offset-2 font-bold text-blue-800">warehouse</span> tab</p>
+        <p> <span class="text-green-700">green</span> bar at the bottom means healthy stock :D </p>
+        <p> <span class="text-red-700">red</span> bar at the bottom means low in stock!!!! time to buy!!! </p>
         <!-- <p> for ez packing every time, pls go to checklist tab !!! </p> -->
         <p> </p>
 
@@ -15,12 +17,7 @@
 </template>
 
 <script>
-// import Button from '../components/Button'
-
 export default {
-    name: 'Home',
-    components: {
-        // Button
-    }
+    name: 'Home'
 }
 </script>
